bb101e9c902e2938717c8131e373dea3ad39a9c9
Carolyn <cgjerde@pixelmill.com>
2017-11-16 16:47:38 -0800

    initial commit - accessibility app

---
+++ my-app/src/components/Dialog/index.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react';
+
+import Disabled from 'ally.js/esm/maintain/disabled';
+import TabFocus from 'ally.js/esm/maintain/tab-focus';
+import Key from 'ally.js/esm/when/key';
+import FirstTab from 'ally.js/esm/query/first-tabbable';
+
+import './styles.css';
+
+interface Props {
+  children: object;
+  title: string;
+  description: string;
+  close(): void;
+}
+
+interface Handle {
+  disengage(): void;
+}
+
+class Dialog extends React.Component<Props, {}> {
+  dialog: HTMLElement | null;
+  disabledHandle: Handle;
+  focusHandle: Handle;
+  keyHandle: Handle;
+  focusedElementBeforeDialogOpened: HTMLInputElement | HTMLButtonElement;
+
+  componentDidMount() {
+    if (document.activeElement instanceof HTMLInputElement ||
+      document.activeElement instanceof HTMLButtonElement) {
+      this.focusedElementBeforeDialogOpened = document.activeElement;
+    }
+
+    this.disabledHandle = Disabled({
+      filter: this.dialog,
+    });
+
+    this.focusHandle = TabFocus({
+      context: this.dialog,
+    });
+
+    this.keyHandle = Key({
+      escape: () => { this.props.close(); },
+    });
+
+    let element = FirstTab({
+      context: this.dialog,
+      defaultToContext: true,
+    });
+    element.focus();
+  }
+  
+  componentWillUnmount() {
+    this.disabledHandle.disengage();
+    this.focusHandle.disengage();
+    this.keyHandle.disengage();
+    this.focusedElementBeforeDialogOpened.focus();
+  }
+
+  render() {
+    return (
+      <div
+        role="dialog"
+        tabIndex={0}
+        className={`popup-outer-container`}
+        aria-hidden={false}
+        aria-labelledby="dialog-title"
+        aria-describedby="dialog-description"
+        ref={(popup) => {
+          this.dialog = popup;
+          }
+        }
+      >
+        <h5 
+          id="dialog-title"
+          className="is-visually-hidden"
+        >
+          {this.props.title}
+        </h5>
+        <p 
+          id="dialog-description"
+          className="is-visually-hidden"
+        >
+          {this.props.description}
+        </p>
+        <div className="popup-inner-container">
+          <button
+            className="close-icon"
+            tabIndex={0}
+            type="button"
+            title="Close Dialog"
+            onClick={() => {
+              this.props.close();
+            }}
+          >
+            ×
+          </button>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dialog;
